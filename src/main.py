e75fd523b7091bbce46e78109f69015dcd648fef
carlobeni04 <carlosbenitez.dev.acct@gmail.com>
2026-02-14 21:44:42 -0300

    Add initial project setup with pyproject.toml and a sample script

---
+++ src/main.py
@@ -0,0 +1,11 @@
+# Codigo Hello World de ejemplo con numpy y matplotlib
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 2 * np.pi, 100)
+y = np.sin(x)
+
+plt.plot(x, y)
+plt.title("Grafica de sin(x)")
+plt.show()
